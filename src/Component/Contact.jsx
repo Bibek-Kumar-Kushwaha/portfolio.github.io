38ded80a0aca94cf1cc00149dff52866133cff23
Bibek Kumar Kushwaha <kushwahabibek9@gmail.com>
2024-06-15 20:37:35 +0545

    add dependencies

--- src/Component/Contact.jsx
+++ src/Component/Contact.jsx
@@ -33,8 +33,7 @@ const Contact = () => {
 
     try {
       const response = await axios.post(
-        // 'http://localhost:3000/contact',
-        '',
+        'http://localhost:3000/contact',
         { name, email, message }, // Pass formData directly without wrapping it
         { headers: { 'Content-Type': 'application/json' } }
       );
